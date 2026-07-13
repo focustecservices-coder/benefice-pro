dfa340f462e81e5c00f4664839498b39e818909a
focustecservices-coder <focustecservices@gmail.com>
2026-07-13 14:51:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-const CACHE_NAME = 'benefice-pro-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png'
-];
-
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-    ))
-  );
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
